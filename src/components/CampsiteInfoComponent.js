00b4297744824f023e22706935defe56e7733b2a
Taylor Masiello <masiellotay@gmail.com>
2021-03-26 01:45:35 -0700

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -17,7 +17,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return(
             <div className="col-md-5 m-1">
@@ -31,7 +31,7 @@ function RenderComments({comments}) {
                         </div>
                     );
                 })}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -54,7 +54,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
@@ -66,7 +70,6 @@ function CampsiteInfo(props) {
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
-const isNumber = val => !isNaN(+val);
 
 class CommentForm extends Component {    
     constructor(props) {
@@ -86,10 +89,9 @@ class CommentForm extends Component {
         });
     }
 
-    handleSubmit(event) {
-        console.log(`Rating: ${event.rating} \n Author: ${event.author} \n Comment: ${event.text}`);
-        alert(
-            `Rating: ${event.rating} \n Author: ${event.author} \n Comment: ${event.text}`);
+    handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
     
     render(){
